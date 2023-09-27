cd1f08fca9f92ac3a711d4d509b6488b6906d61b
marcelobiao <biao.marcelo@gmail.com>
2023-09-27 13:54:21 -0300

    feat: creating producthandler and add chi router

--- 7-api/cmd/server/main.go
+++ 7-api/cmd/server/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/marcelobiao/fullcycle-go-expert/7-api/configs"
+	"github.com/marcelobiao/fullcycle-go-expert/7-api/internal/entity"
+	"github.com/marcelobiao/fullcycle-go-expert/7-api/internal/infra/database"
+	"github.com/marcelobiao/fullcycle-go-expert/7-api/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
-	config, _ := configs.LoadConfig(".")
-	fmt.Println(config)
+	if _, err := configs.LoadConfig("."); err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8000", r)
 }
---
+++ 7-api/internal/infra/webserver/handlers/product_handlers.go
@@ -0,0 +1,53 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/marcelobiao/fullcycle-go-expert/7-api/internal/dto"
+	"github.com/marcelobiao/fullcycle-go-expert/7-api/internal/entity"
+	"github.com/marcelobiao/fullcycle-go-expert/7-api/internal/infra/database"
+)
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		msg := struct {
+			Message string `json:"message"`
+		}{
+			Message: err.Error(),
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(msg)
+		return
+	}
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		msg := struct {
+			Message string `json:"message"`
+		}{
+			Message: err.Error(),
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(msg)
+		return
+	}
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
